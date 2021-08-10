cd076281e6b6a56557b120cd38987425592f6d98
Reese Ingraham <reese.ingraham@gmail.com>
2021-08-10 10:22:07 -0700

    changed jupyterhub realm name -> hale-bopp

--- README.md
+++ README.md
@@ -1,26 +1,39 @@
 # Quasi-Enterprise JupyterHub Deployment
 
-This project enables an aspiring person to run an example "Science Platform" suitable for astrophysics research.
-If all you need is a local JupyterLab notebook with Python 3.7 and the amazing astronomy and processing libraries then this might be overkill.
-However, if you want to leverage more of an ecosystem around those components with JupyterHub then this can be a springboard to launch your own work.
+Welcome to a small project that enables an aspiring person to run a
+"Science Platform" suitable for astrophysics research. This might be overkill
+if all you need is a local JupyterLab notebook with Python 3.7 (and the amazing
+astronomy and processing libraries). However, if you want to leverage more of
+an ecosystem around those components with JupyterHub then this can be a
+springboard to launch your own project.
 
-Spin-up a JupyterHub server with these features:
+If you're looking for a large enterprise-scale JupyterHub deployment running on
+Kubernetes then checkout [Zero-to-JupyterHub
+with k8s](https://zero-to-jupyterhub.readthedocs.io/en/latest/index.html)
+
+### Spin-up a JupyterHub server with these features
 * Reverse-Proxy
 * Industry-standard SSO Authentication
-* SQL database for persisting configurations and for SQL-based workflows
-* Mock S3 storage (future)
+* SQL database for persistent configurations and for SQL-based workflows
+* Mock S3 object store
 * Full-suite of the common Python astrophysics & astronomical libraries
+* Spark processing cluster (future)
 
-Loosely based on the deployment in use at [Université de
-Versailles](https://jupyter.ens.uvsq.fr/) which is described in depth in [this blog
-post](https://opendreamkit.org/2018/10/17/jupyterhub-docker/).
+Loosely based on the documented [JupyterHub
+deployment](https://github.com/defeo/jupyterhub-docker/) at [Université de
+Versailles](https://jupyter.ens.uvsq.fr/) which is described in depth in
+[this blog post](https://opendreamkit.org/2018/10/17/jupyterhub-docker/).
 
+Additionally look up the official [JupyterHub Docker
+Deploy](https://github.com/jupyterhub/jupyterhub-deploy-docker) for more
+JupyterHub+Docker information.
 
-### List of All-🌟 Projects making this demonstration possible
-* [Docker]()
-* [Keycloak]()
-* [Python]()
-* [Postgres]()
+### All-🌟 projects making this demonstration possible
+* [Docker](https://www.docker.com)
+* [Keycloak](https://www.keycloak.org)
+* [Python](https://www.python.org)
+* [Postgres](https://www.postgresql.org)
+* [Zenko CloudServer](https://s3-server.readthedocs.io/en/latest/)
 * [JupyterHub](https://jupyter.org/hub)
 
 
@@ -29,12 +42,15 @@ post](https://opendreamkit.org/2018/10/17/jupyterhub-docker/).
 
 ### Adapt to your needs
 
-This deployment is ready to clone and roll on your own server. Read
-the [blog
-post](https://opendreamkit.org/2018/10/17/jupyterhub-docker/) first,
-to be sure you understand the configuration.
+This deployment is ready for you to explore. But if you want to clone and roll
+on your own server read below!
 
-Then, if you like, clone this repository and apply (at least) the
+
+_Disclaimer: ensure you understand the well-written by outdated [blog
+post](https://opendreamkit.org/2018/10/17/jupyterhub-docker/) first,
+to be sure you understand the configuration._
+
+Then, if you like, clone this repository and look into making (at least) the
 following changes:
 
 - In [`.env`](.env), set the variable `HOST` to the name of the server you
@@ -42,29 +58,39 @@ following changes:
 - In [`reverse-proxy/traefik.toml`](reverse-proxy/traefik.toml), edit
   the paths in `certFile` and `keyFile` and point them to your own TLS
   certificates. Possibly edit the `volumes` section in the
-  `reverse-proyx` service in
+  `reverse-proxy` service in
   [`docker-compose.yml`](docker-compose.yml).
 - In
   [`jupyterhub/jupyterhub_config.py`](jupyterhub/jupyterhub_config.py),
-  edit the *"Authenticator"* section according to your institution
-  authentication server.  If in doubt, [read
+  edit the *"Authenticator"* section according to your authentication server.
+  If in doubt, [read
   here](https://jupyterhub.readthedocs.io/en/stable/getting-started/authenticators-users-basics.html).
 
 Other changes you may like to make:
 
-- Edit [`jupyterlab/Dockerfile`](jupyterlab/Dockerfile) to include the
+- Edit [`jupyterlab/Dockerfile`](jupyterlab/Dockerfile) to include different
   software you like. Do not forget to change
   [`jupyterhub/jupyterhub_config.py`](jupyterhub/jupyterhub_config.py)
   accordingly, in particular the *"user data persistence"* section.
 
-### Run!
+## Run!
 
-Once you are ready, build and launch the application with
+Once you are ready, build and launch the application with:
 
 ```
 docker-compose build
 docker-compose up -d
 ```
+Then navigate to [jupyterhub.docker.localhost](http://jupyterhub.docker.localhost/)
+(Google Chrome might work better than other browsers, YMMV)
+
+## Stop
+
+```
+docker-compose down
+```
+
+## More
 
 Read the [Docker Compose manual](https://docs.docker.com/compose/) to
 learn how to manage your application.
--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,46 +1,116 @@
 # JupyterHub configuration
 #
-# If you update this file, do not forget to delete the `jupyterhub_data` volume before restarting the jupyterhub service:
+# If you update this file, do not forget to delete the Jupyter Hub Docker data volume before restarting the jupyterhub service:
 #
-#     docker volume rm hale-bopp_jupyterhub_data
+#     docker volume rm hale-bopp_hub-data
 
 import os
+import sys
 
-# Generic
-c.JupyterHub.admin_access = True
+from jupyter_client.localinterfaces import public_ips
+from jupyterhub.handlers.login import LogoutHandler
+from oauthenticator.generic import GenericOAuthenticator
+from oauthenticator.oauth2 import OAuthLoginHandler
+from tornado.auth import OAuth2Mixin
+from tornado.httputil import url_concat
+from traitlets import Unicode
+
+
+def construct_db_conn_string(spawner):
+    username = spawner.user.name
+    return f"postgres://{username}:{username}@{os.environ['POSTGRES_HOST']}:{os.environ['POSTGRES_PORT']}/{username}"
+
+
+def construct_bucket_name(spawner):
+    username = spawner.user.name
+    return f"s3://{username}"
+
+# Reference Links
+#    https://jupyterhub-dockerspawner.readthedocs.io/en/latest/api/index.html
+#    https://jupyterhub.readthedocs.io/en/stable/api/app.html
+
+
+# Baseline
 c.Spawner.default_url = "/lab"
+c.JupyterHub.allow_named_servers = True
+c.JupyterHub.named_server_limit_per_user = 3
+c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
+c.JupyterHub.shutdown_on_logout = True  # Good for demo purposes. Most likely not desirable for production
+
 
 ### Authentication per https://oauthenticator.readthedocs.io/en/stable/getting-started.html
-from oauthenticator.oauth2 import OAuthLoginHandler
-from oauthenticator.generic import GenericOAuthenticator
-from tornado.auth import OAuth2Mixin
-
-# OAuth2 endpoints
 class MyOAuthMixin(OAuth2Mixin):
-    _OAUTH_AUTHORIZE_URL = 'http://localhost:8888/auth/realms/jupyterhub/protocol/openid-connect/auth'
-    _OAUTH_ACCESS_TOKEN_URL = 'http://localhost:8888/auth/realms/jupyterhub/protocol/openid-connect/token'
+    _OAUTH_AUTHORIZE_URL = os.environ["OAUTH_AUTHORIZE_URL"]
+    _OAUTH_ACCESS_TOKEN_URL = os.environ["OAUTH_ACCESS_TOKEN_URL"]
+
 
 class MyOAuthLoginHandler(OAuthLoginHandler, MyOAuthMixin):
     pass
 
-# Authenticator configuration
-class MyOAuthAuthenticator(GenericOAuthenticator):
-    login_service = 'keycloak'
+
+class KeycloakLogoutHandler(LogoutHandler):
+    """Logout handler for keycloak"""
+
+    async def render_logout_page(self):
+        params = {
+            "redirect_uri": f"{self.request.protocol}://{self.request.host}{self.hub.server.base_url}"
+        }
+        self.redirect(
+            url_concat(self.authenticator.keycloak_logout_url, params),
+            permanent=False
+        )
+
+
+class KeycloakAuthenticator(GenericOAuthenticator):
+    login_service = "Keycloak SSO"
     login_handler = MyOAuthLoginHandler
-    userdata_url = 'http://localhost:8888/auth/realms/jupyterhub/protocol/openid-connect/userinfo'
-    token_url = 'http://localhost:8888/auth/realms/jupyterhub/protocol/openid-connect/token'
-    oauth_callback_url = 'http://jupyter.docker.localhost/hub/oauth_callback'
-    client_secret = '181504b4-fc17-486f-8023-be5759d4e3d6'
-    client_id = 'jupyterhub'
+    userdata_url = os.environ["OAUTH_USERDATA_URL"]
+    token_url = os.environ["OAUTH_ACCESS_TOKEN_URL"]
+    oauth_callback_url = os.environ["OAUTH_CALLBACK_URL"]
+    client_secret = os.environ["OAUTH_CLIENT_SECRET"]
+    client_id = os.environ["OAUTH_CLIENT_ID"]
 
-c.JupyterHub.authenticator_class = MyOAuthAuthenticator
+    keycloak_logout_url = Unicode(
+        config=True,
+        help="The keycloak logout URL"
+    )
 
-# Docker spawner
-c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
+    def get_handlers(self, app):
+        return super().get_handlers(app) + [(r'/logout', KeycloakLogoutHandler)]
+
+
+c.JupyterHub.authenticator_class = KeycloakAuthenticator
+c.KeycloakAuthenticator.keycloak_logout_url = os.environ["KEYCLOAK_LOGOUT_URL"]
+
+
+# Users
+# c.Authenticator.whitelist = whitelist = set()
+# c.Authenticator.admin_users = admin = set()
+# c.Authenticator.admin_users = {"tonysappe", "admin"}
+# c.Authenticator.whitelist = {"tonysappe", "admin", "dave", "tom"}
+c.JupyterHub.admin_access = True
+
+
+# Docker Spawner
 c.DockerSpawner.image = os.environ["DOCKER_JUPYTER_CONTAINER"]
+c.DockerSpawner.prefix = "jupyterlab"
+c.DockerSpawner.name_template = "jupyterlab-{username}-notebooks-{servername}"
+c.DockerSpawner.remove = True  # Default: False (If True, delete containers when servers are stopped)
+# c.DockerSpawner.volumes  # Default: None (Map from host file/directory to container (guest) file/directory mount point and (optionally) a mode)
+# c.DockerSpawner.pre_spawn_hook  # An optional hook function that you can implement to do some bootstrapping work before the spawner starts
+
+# Networking
 c.DockerSpawner.network_name = os.environ["DOCKER_NETWORK_NAME"]
+c.DockerSpawner.extra_host_config = {"network_mode": os.environ["DOCKER_NETWORK_NAME"]}
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
-c.JupyterHub.hub_ip = os.environ["HUB_IP"]
+c.JupyterHub.hub_ip = public_ips()[0]
+
+c.DockerSpawner.env_keep = ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "BLOB_STORE_URL"]
+c.DockerSpawner.environment = {
+    # database_url = os.environ["DATABASE_URL"].format(os.environ)
+    "DATABASE_URL": construct_db_conn_string,
+    "BLOB_STORE_BUCKET": construct_bucket_name,
+}
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
@@ -48,16 +118,80 @@ notebook_dir = os.environ.get("DOCKER_NOTEBOOK_DIR") or "/home/jovyan"
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir}
 
-# Other stuff
-c.Spawner.cpu_limit = 1
-c.Spawner.mem_limit = "10G"
-
+# Limits and Guarentees
+c.JupyterHub.concurrent_spawn_limit = 5  # Default: 100 (Max number of concurrent users that can be spawning at a time)
+c.JupyterHub.active_server_limit = 10
+c.Spawner.cpu_limit = 4                  # Default: None (Max number of cpu-cores a single-user notebook server is allowed to use)
+c.Spawner.cpu_guarantee = 1              # Default: None (Min number of cpu-cores a single-user notebook server is guaranteed to have available)
+c.Spawner.mem_limit = "10G"              # Default: None (Maximum number of bytes a single-user notebook server is allowed to use)
+c.Spawner.mem_guarantee = "1G"           # Default: None (Min number of bytes a single-user notebook server is guaranteed to have available)
 
 # Services
 c.JupyterHub.services = [
     {
-        "name": "cull_idle",
+        "name": "idle-culler",
         "admin": True,
-        "command": "python /srv/jupyterhub/cull_idle_servers.py --timeout=3600".split(),
+        "command": [
+            sys.executable,
+            "-m",
+            "jupyterhub_idle_culler",
+            f"--timeout={os.environ['CONTAINER_IDLE_TIMEOUT']}"
+        ],
     },
 ]
+
+
+# Hooks
+def hook_runner(spawner):
+    database_init(spawner)
+    blobstore_init(spawner)
+
+
+def database_init(spawner):
+    username = spawner.user.name
+
+    # BAD Practice to insert user-provided strings like this! DockerSpawner tries to replace %s
+    check_user = f"SELECT * FROM pg_catalog.pg_user WHERE usename = '{username}'"
+    create_user = f"CREATE USER {username} WITH PASSWORD '{username}'"
+    check_db = f"SELECT * FROM pg_database WHERE datname = '{username}'"
+    create_db = f"CREATE DATABASE {username} WITH OWNER = '{username}'"
+    revoke_all = f"REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM {username}"
+    grant_priv = f"GRANT ALL PRIVILEGES ON DATABASE {username} TO {username}"
+
+    import psycopg2
+    connection = psycopg2.connect(dsn=os.environ["DATABASE_URL"])
+    connection.autocommit = True
+    cursor = connection.cursor()
+    cursor.execute(check_user)
+    if not cursor.fetchone():
+        cursor.execute(create_user)
+    cursor.execute(check_db)
+    if not cursor.fetchone():
+        cursor.execute(create_db)
+    cursor.execute(revoke_all)
+    cursor.execute(grant_priv)
+
+    connection.commit()
+    connection.close()
+
+
+def blobstore_init(spawner):
+    username = spawner.user.name
+    import boto3
+    import os
+
+    client = boto3.client(
+        's3',
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        endpoint_url=os.environ["BLOB_STORE_URL"]
+    )
+    buckets = client.list_buckets()
+    for bucket in buckets["Buckets"]:
+        if bucket["Name"] == username:
+            break
+    else:
+        client.create_bucket(Bucket=username)
+
+
+c.Spawner.pre_spawn_hook = hook_runner
